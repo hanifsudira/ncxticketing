c3d56fd8ef95e556ac6a148690c17b8c98f6c0f0
emfatoni <em.fatoni@gmail.com>
2017-12-09 10:47:24 +0700

    tambah storeTicket()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Ticket;
 
 use Illuminate\Http\Request;
 
@@ -21,5 +22,40 @@ class UserController extends Controller
         return view('user.home');
         //$data = auth()->user()->role;
         //var_dump($data);
+    }
+
+
+    // fungsi membuat tiket baru
+    public function storeTicket(Request $req){
+        $now = new DateTime();
+
+        // assigning
+        $temp_ticket = new Ticket();
+
+        
+        $temp_ticket->konten = $req->konten;
+        $temp_ticket->assignee = $req->assignee;
+        $temp_ticket->id_jenis = $req->id_jenis;
+        $temp_ticket->no_order = $req->no_order;
+        $temp_ticket->segmen = $req->segmen;
+        $temp_ticket->author = $req->author;
+        
+        $temp_ticket->tanggal = $now->getTimestamp();
+        $temp_ticket->status = "Pending";
+        $temp_ticket->url_gambar = null;
+        $temp_ticket->is_root = "Y";
+        $temp_ticket->id_root = null;
+        $temp_ticket->tanggal_complete = null;
+        $temp_ticket->prev_ticket = null;
+        $temp_ticket->detail_order = null;
+
+        // save to db
+        return $temp_ticket->save() ? TRUE : FALSE;
+    }
+
+    // fungsi transfer tiket: eskalasi, request complete, return to user
+    public function transferTicket(){
+        $now = new DateTime();
+    //     
     }
 }
